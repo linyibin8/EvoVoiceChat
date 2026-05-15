173c8b6a3d92657c8ca9f5a549bb776dc1783f76
linyibin8 <269123786@qq.com>
2026-05-15 15:12:21 +0800

    Keep iOS app awake

--- ios/EvoVoiceChat/EvoVoiceChatApp.swift
+++ ios/EvoVoiceChat/EvoVoiceChatApp.swift
@@ -1,13 +1,25 @@
 import SwiftUI
+import UIKit
 
 @main
 struct EvoVoiceChatApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     @StateObject private var settings = AppSettings()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(settings)
+                .onAppear {
+                    setIdleTimerDisabled(true)
+                }
+                .onChange(of: scenePhase) { _, newPhase in
+                    setIdleTimerDisabled(newPhase == .active)
+                }
         }
+    }
+
+    private func setIdleTimerDisabled(_ isDisabled: Bool) {
+        UIApplication.shared.isIdleTimerDisabled = isDisabled
     }
 }
